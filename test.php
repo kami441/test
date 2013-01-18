e9f2589d1d551bf93b792685585bb1b64062a93a
vero <runneuse64@gmail.com>
2013-01-18 16:10:08 +0100

    Ajout d'un paragraphe
    1er paragraphe

--- test.php
+++ test.php
@@ -6,5 +6,7 @@
 </head>
 <body>
 	<h1><?php echo 'Hello world'; ?></h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem doloremque veritatis aspernatur amet ullam ab velit molestiae earum nesciunt ea laborum quibusdam officia placeat debitis inventore ipsum a esse architecto.</p>
+	
 </body>
 </html>
